110c8503015526a118ddb4e190bc65159c78e567
TheCeolo <pietro.ceolotto@gmail.com>
2023-04-26 21:41:36 +0200

    ffadsa

--- response.php
+++ response.php
@@ -5,7 +5,7 @@ $a = array();
 array_push($a,array("type"=>0,"content"=>'My Title',"bold"=>1,"align"=>2,"format"=>3));
 
 array_push($a,array("type"=>1,"path"=>'https://it.m.wikipedia.org/wiki/File:Circle_-_black_simple.svg',"align"=>2));
-
+header('Content-Type: application/json; charset=utf-8');
 echo json_encode($a,JSON_FORCE_OBJECT);
 
 ?>
